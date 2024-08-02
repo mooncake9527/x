c114cf7e84afb5d67f3314e8a3a520c0548a64d2
mooncake9527 <scotttu@163.com>
2024-08-02 16:11:30 +0800

    xip

---
+++ xip/xip.go
@@ -0,0 +1,60 @@
+package xip
+
+import "net"
+
+// GetServerIp 获取服务端 IP
+// 问题：我在本地多网卡机器上，运行分布式场景，此函数返回的ip有误导致rpc连接失败。 遂google结果如下：
+// 1、https://www.jianshu.com/p/301aabc06972
+// 2、https://www.cnblogs.com/chaselogs/p/11301940.html
+func GetServerIp() string {
+	ip, err := externalIP()
+	if err != nil {
+		return ""
+	}
+	return ip.String()
+}
+
+func externalIP() (net.IP, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, err
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
+	}
+	return ip
+}
---
+++ xip/xip_test.go
@@ -0,0 +1,21 @@
+package xip
+
+import "testing"
+
+func TestGetServerIp(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{name: "local ip", want: "192.168.31.95"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetServerIp()
+			t.Log("got", got)
+			if got != tt.want {
+				t.Errorf("GetServerIp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
