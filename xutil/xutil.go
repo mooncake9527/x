c7c5ac9999e42b1490240c0989c3ad1ac904890a
mooncake9527 <scotttu@163.com>
2024-08-05 09:36:53 +0800

    u

--- xutil/xutil.go
+++ xutil/xutil.go
@@ -1,7 +1,6 @@
 package xutil
 
 // InArray 将在数组中搜索任意类型的元素，返回匹配元素的布尔值。
-// element 是要搜索的元素，collection 是要搜索的值的切片。
 func InArray[T comparable](element T, collection []T) bool {
 	for _, item := range collection {
 		if item == element {
